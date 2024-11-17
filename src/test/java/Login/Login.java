75529049dd36ffd28bca32f9c80ea92c209f5ea2
razamd260 <razamd260@gmail.com>
2024-11-17 14:47:07 +0530

    2:47

--- src/test/java/Login/Login.java
+++ src/test/java/Login/Login.java
@@ -7,6 +7,8 @@ public class Login {
 		@Test (groups = "smoke")
 		public void Mtest() {
 		   System.out.println("Login Smoke Testing..");
+		   System.out.println("smoke time 7:14");
+		   System.out.println("smoke2:32PM");
 		}
 	
 	   
